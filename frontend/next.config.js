ceb826a1e9de79b3570d727ed8ef5abc42191b57
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-10-31 10:49:33 +0100

    okay here

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 module.exports = () => {
     const rewrites = () => {
-        const SERVER_URL = process.env.BACKEND_PROXY || "http://localhost:5000";
+        // dirty fix, locally doesn't work anymore
+        const SERVER_URL = process.env.BACKEND_PROXY || "http://asvz-graph-backend:5000";
         return [{
             source: "/api/:path*",
             destination: `${SERVER_URL}/api/:path*`,
