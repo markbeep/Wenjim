c281619163c6f4279c24e8bc2bc73842896f8892
markbeep <mrkswrn@gmail.com>
2023-02-24 23:38:57 +0100

    dockerfile correctly builds website now

--- frontend/compile-proto.sh
+++ frontend/compile-proto.sh
@@ -2,14 +2,14 @@
 
 OUT_DIR="."
 TS_OUT_DIR="."
-IN_DIR="../proto/generated"
-PROTOC="$(npm bin)/grpc_tools_node_protoc"
-PROTOC_GEN_GRPC_WEB_PATH="$(npm bin)/protoc-gen-grpc-web"
+IN_DIR="../proto"
+PROTOC="node_modules/.bin/grpc_tools_node_protoc"
+PROTOC_GEN_GRPC_WEB_PATH="node_modules/.bin/protoc-gen-grpc-web"
 
 # generate grpc-web files & types
 $PROTOC \
-    -I="../proto" \
+    -I=$IN_DIR \
     --plugin=protoc-gen-grpc-web=${PROTOC_GEN_GRPC_WEB_PATH} \
     --js_out=import_style=commonjs:$OUT_DIR \
     --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:$OUT_DIR \
-    "$IN_DIR"/*.proto
+    $IN_DIR/generated/*.proto
