f9fd3f062de643ea463c0c6b013b3d9a69a73a17
Mark <mrkswrn@gmail.com>
2023-05-03 09:31:00 +0200

    Cleaner backend without envoy proxy

--- backend/server.py
+++ backend/server.py
@@ -1,31 +1,22 @@
-"""General backend server handling using REST API to communicate"""
+"""General backend server handling using GRPC-Web to communicate"""
 
-from concurrent import futures
 import logging
-import grpc
+
+from flask import Flask
+from sonora.wsgi import grpcWSGI
+
 from generated import countday_pb2_grpc
 from history import HistoryServicer
 from utility import UtilityServicer
 from weekly import WeeklyServicer
 
-def serve():
-    port = 9090
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    countday_pb2_grpc.add_UtilityServicer_to_server(UtilityServicer(), server)
-    countday_pb2_grpc.add_HistoryServicer_to_server(HistoryServicer(), server)
-    countday_pb2_grpc.add_WeeklyServicer_to_server(WeeklyServicer(), server)
-    server.add_insecure_port(f"[::]:{port}")
-    server.start()
-    logging.log(logging.INFO, "Listening on %d", port)
-    server.wait_for_termination()
+app = Flask(__name__)
+app = grpcWSGI(app)
+countday_pb2_grpc.add_UtilityServicer_to_server(UtilityServicer(), app)
+countday_pb2_grpc.add_HistoryServicer_to_server(HistoryServicer(), app)
+countday_pb2_grpc.add_WeeklyServicer_to_server(WeeklyServicer(), app)
 
-
-def main():
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger("peewee")
-    logger.addHandler(logging.StreamHandler())
-    logger.setLevel(level=logging.DEBUG)
-    serve()
-
-if __name__ == "__main__":
-    main()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("peewee")
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(level=logging.DEBUG)
--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -4,6 +4,6 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
     // proxies api requests to the envoy service
     if (request.nextUrl.pathname.startsWith("/api")) {
-        return NextResponse.rewrite(new URL(request.nextUrl.pathname, `${process.env.ENVOY_PROXY}`));
+        return NextResponse.rewrite(new URL(request.nextUrl.pathname.replace("/api/", ""), `${process.env.ENVOY_PROXY}`));
     }
 }
