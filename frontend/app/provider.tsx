188c918a324cd6ca2ba8a44d3131213618a0f4a5
Markbeep <mrkswrn@gmail.com>
2023-09-16 14:29:51 +0200

    update to tanstack

--- frontend/app/provider.tsx
+++ frontend/app/provider.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React, { useState } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThemeProvider from "../components/themeProvider";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Shell from "./shell";
 
 export default function Provider({ children }: React.PropsWithChildren) {
